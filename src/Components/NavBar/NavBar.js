75b4ac89fd87f7d2d00e332e9e19b98375c47866
arumsey93 <50844307+arumsey93@users.noreply.github.com>
2019-08-06 15:54:56 -0500

    Revert "Finishingtouches"

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,19 +3,9 @@ import { Link, withRouter } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 class NavBar extends Component {
-    state = {
-        saveDisabled: false
-    }
-
-    logOut = () => {
-        sessionStorage.clear()
-        this.props.history.push("welcome/login")
-    }
-    
-
     render () {
         return (
-            <nav className="navbar justify-content-center navbar-dark light-blue bg-dark flex-md-nowrap p-0 shadow">
+            <nav className="navbar justify-content-center navbar-light light- bg-dark flex-md-nowrap p-0 shadow">
             <ul className="nav nav-pills nav-fill">
                 <li className="nav-item">
                     <Link className="nav-link" to="/">Dashboard</Link>
@@ -28,15 +18,6 @@ class NavBar extends Component {
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to="/hallOfFame">Hall of Fame</Link>
-                </li>
-                <li className="nav-item">
-                    <button 
-                    className="btn btn-dark light-blue bg-dark shadow"
-                    onClick={() => {
-                        this.setState({saveDisabled: true})
-                        this.logOut()
-                    }}
-                    >Log Out</button>
                 </li>
             </ul>
             </nav>
