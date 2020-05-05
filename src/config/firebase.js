df092f554035ffd3bba3b2d52c77f59cafed6930
Alex rumsey <alexrumsey@Alexs-MacBook-Pro.local>
2020-05-05 16:10:42 -0500

    firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCi7CK-0RjwJUr6MeUcwFw3g1N_prXhOL0",
+  authDomain: "movie-hero-3573a.firebaseapp.com",
+  databaseURL: "https://movie-hero-3573a.firebaseio.com",
+  projectId: "movie-hero-3573a",
+  storageBucket: "movie-hero-3573a.appspot.com",
+  messagingSenderId: "945597636212",
+  appId: "1:945597636212:web:f608d4076b5d996a"
+};
